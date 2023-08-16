acc42685435a9744cc631eeb95c021515bdb13ba
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-08-16 18:49:54 -0300

    Create index.php

---
+++ ex_treino/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testando o PHP</title>
+</head>
+<body>
+    <header>
+        <h1>Variáveis em PHP</h1>
+    </header>
+    
+    <?php 
+        $nome = "Raphael";
+
+        echo "$nome";
+    ?>
+
+</body>
+</html>
