06f7f38824eb929c99fb83fb145a2094c8ffb28f
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-08-16 15:02:58 -0300

    Update index.php

--- ex003/index.php
+++ ex003/index.php
@@ -13,7 +13,11 @@
 <body>
     <h1>Tipos Primitivos em PHP</h1>
     <?php 
+        $num = 300;
+        echo "O valor da variável é $num.";
 
+        $num_two = 0x1A; //bases numéricas -> 0x = hexadecimal, 0b = binário , 0 = Octal
+        echo "O valor da variável é $num_two.";
     ?>
     
 </body>
