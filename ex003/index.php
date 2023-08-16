d5ce137e6be6208d3c0417913bf85ba12d2102e0
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-08-16 14:58:12 -0300

    Create index.php

---
+++ ex003/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos Primitivos</title>
+    <style>
+        body{
+            background-color: lightblue;
+        }
+    </style>
+</head>
+<body>
+    <h1>Tipos Primitivos em PHP</h1>
+    <?php 
+
+    ?>
+    
+</body>
+</html>
