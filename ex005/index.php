7b5cf2e387c22f1d36c12dd1174b1438514d603a
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-08-18 18:17:44 -0300

    Update index.php

---
+++ ex005/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Expressões Aritméticas do PHP</title>
+</head>
+<body>
+<header>
+        <h1>Resultado da Operação</h1>
+    </header>
+    <main>
+        <?php //a ordem de precedência de Operação é a mesma da Matemática
+            $res = 5 + 2 / 2;
+            echo "$res";
+        ?>
+    </main>
+</body>
+</html>
