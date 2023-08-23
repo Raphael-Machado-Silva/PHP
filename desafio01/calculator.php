5a7df2197f49aa7dadccac52e4e0e2ea3128b710
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-08-23 10:49:41 -0300

    Update calculator.php

--- desafio01/calculator.php
+++ desafio01/calculator.php
@@ -3,20 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Número</title>
 </head>
 <body>
     <main>
-        <h1>O seu sucessor e antecessor é:</h1>
+        <h1>Resultado Final:</h1>
         <?php 
-        $num= $_REQUEST['num'];
+        $num= $_REQUEST['num'] ?? 0;
 
         $antecessor= $num -1;
         $sucessor= $num +1;
 
         echo "O número escolhido foi $num";
-        echo "Seu sucessor é $sucessor.";
-        echo "Seu antecessor é $antecessor.";
+        echo "<br>Seu sucessor é $sucessor.";
+        echo "<br>Seu antecessor é $antecessor.";
         ?>
     </main>
     
