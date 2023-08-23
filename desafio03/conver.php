9b5e14a6e8aee12c1089b001aec8bcf6c90765e3
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-08-23 19:36:42 -0300

    Update conver.php

--- desafio03/conver.php
+++ desafio03/conver.php
@@ -10,15 +10,23 @@
     <main>
         <h1 style="text-align: center;">Resultado Final</h1>
         <?php 
+        /* ------------MEU JEITO DE FAZER-----------
         $valor= $_REQUEST['valor'];
         $dolar = $valor / 4.86;
 
-        if($valor == " " || $valor == 0){
+        if(empty($valor) || $valor == 0){
             echo "Por favor volte para a página anterior e insira um valor!!";
         } else{        
             echo "Os seus $valor equivalem a: <br>";
             echo "Você possui U$ " . number_format($dolar,2) . ".";}
+*/
 
+        $cotacao = 4.86;
+        $real = $_REQUEST['valor'];
+        $dolar = $real / $cotacao;
+
+        $padrao = numfmt_create("pt-BR", NumberFormatter::CURRENCY);
+        echo "<p> Seus " . numfmt_format_currency($padrao, $real, "BRL") . " equivalem a " . numfmt_format_currency($padrao, $dolar, "USD") . ".</p>";
         ?>
     <button><a href="index.html">&#x2B05; Voltar</a></button>
     </main>
