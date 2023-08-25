11ddfabf484f240060ecc0823545165c28a2cfc8
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-08-25 10:50:56 -0300

    Update form_alimen.php

--- ex007/form_alimen.php
+++ ex007/form_alimen.php
@@ -12,14 +12,32 @@
         $valor1 = $_GET['v1'] ?? 0;
         $valor2 = $_GET['v2'] ?? 0;
     ?>
-    <main>   <!--Método de chamar no action a própria página-->
+    <main>
+        <h1 style="text-align: center;">Somador de Valores</h1>   
+
+                <!--Método de chamar no action a própria página-->
         <form action="<?php echo $_SERVER['PHP_SELF']?>" method="get">
+
              <label for="v1">Valor 1:</label>
-             <input type="number" name="v1" id="v1">
+             <input type="number" name="v1" id="v1" value="<?php echo $valor1; ?>">
+             <!-- Esse VALUE é para os imputs nao ficarem em branco ao clicar no button -->
              <label for="v2">Valor 2:</label>
-             <input type="number" name="v2" id="v2">
+             <input type="number" name="v2" id="v2" value="<?php echo $valor2 ?>">
+
              <input type="submit" value="Somar">
         </form>
     </main>
+    <section id="resultado">
+        <h2>Resultado da Soma:</h2>
+        <?php 
+        $soma = $valor1 + $valor2;
+            if($soma == 0 || $soma == null){
+            
+            } else{          
+            echo "O valor da soma é: " . "<strong>$soma</strong>". ".";
+            }
+
+        ?>
+    </section>
 </body>
 </html>
