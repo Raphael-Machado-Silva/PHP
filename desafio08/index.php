b9e5dc19542c80d50b3173828b00951063017b1d
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-08-28 16:08:19 -0300

    Update index.php

--- desafio08/index.php
+++ desafio08/index.php
@@ -14,18 +14,24 @@
         <h1 style="text-align: center;">Informe um  Número</h1>
         <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
             <label for="num">Número:</label>
-            <input type="number" name="num" id="idnum" value="<?php echo $num ?>">
+            <input type="number" name="num" id="idnum" value="<?php echo $num ?>" min="1" step="any" required>
             <input type="submit" value="Calcular Raízes">
         </form>
     </main>
     <article>
         <h2 style="text-align: center;">Resultado Final</h2>
         <?php 
-        $cubica = pow($num, 1/3);
-        $quadrada = sqrt($num);
-        echo "Analisando o <strong> número " . $num . "</strong>, temos:";
-        echo "<ul> <li>A sua raiz quadrada é ". "<strong>". number_format($quadrada, 2) . "</strong>;";
-        echo "<li>A sua raiz cúbica é "."<strong>".number_format($cubica,2 )."</strong>; </ul>";
+
+        if($num ==0){
+            echo "<p> Insira um valor inteiro acima!</p>";
+        } else {
+            $cubica = pow($num, 1/3);
+            $quadrada = sqrt($num);
+                echo "Analisando o <strong> número " . $num . "</strong>, temos:";
+                echo "<ul> <li>A sua raiz quadrada é ". "<strong>". number_format($quadrada, 2) . "</strong>;";
+                echo "<li>A sua raiz cúbica é "."<strong>".number_format($cubica,2 )."</strong>; </ul>";
+        }
+
         ?>
     </article>
 </body>
