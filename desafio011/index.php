b834b9ab381b4f3943d5e95ffac8c3963809c1f1
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-08-28 19:43:18 -0300

    Update index.php

--- desafio011/index.php
+++ desafio011/index.php
@@ -15,7 +15,7 @@
         <h1 style="text-align: center;">Reajustador de Preços</h1>
         <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
             <label for="preco">Preço do Produto (R$)</label>
-            <input type="number" name="preco" id="idpreco" min="0.1" step="0.01">
+            <input type="number" name="preco" id="idpreco" min="0.1" step="0.01" value=" " required>
 
             <label for="reajust">Qual será o percentual de reajuste? (<strong><span id="p">?</span>%</strong>)</label>
             <input type="range" name="reajust" id="idreajust" min="0%" max="100%" value="50%" step="0.1" oninput="mudaValor()">
@@ -26,8 +26,13 @@
     <article>
         <h2 style="text-align: center;">Resultado do Reajuste</h2>
         <?php 
-        $aumento = ($preco * $reajust)/100 + $preco;
-        echo "O produto que custava R$". number_format($preco,2). ", com ". $reajust. "% de aumento vai passar a custar R$". number_format($aumento,2). " a partir de agora.";
+        if($preco == 0){
+            echo "Insira um valor positivo acima!";
+        } else{
+            $aumento = ($preco * $reajust)/100 + $preco;
+            echo "O produto que custava R$". number_format($preco,2). ", com ". $reajust. "% de aumento vai passar a custar R$". number_format($aumento,2). " a partir de agora.";
+        }
+
         ?>
     </article>
     <script>
