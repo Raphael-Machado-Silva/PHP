4be188c3ea51b14ebda0f6e74ae906a82c1e622b
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-09-07 17:37:22 -0300

    Update index.php

--- php_poo/namespaces/index.php
+++ php_poo/namespaces/index.php
@@ -4,8 +4,15 @@
 require 'classes/produto.php';
 require 'models/produto.php';
 
-$produto = new \classes\Produto(); //para evitar conflitos de arquivos com nomes iguais, usa-se os namespaces
+
+//para evitar conflitos de arquivos com nomes iguais, usa-se os namespaces, ele são declarados pelo REQUIRE e uso das CONTRA BARRAS
+$produto = new \classes\Produto(); 
 $produto->mostrarDetalhes();
 
 $produto02 = new \models\Produto();
 $produto->mostrarDetalhes();
+
+
+/* também poderia ser chamado destá maneira: user models/Produto
+retirando a necessidade da contra barra; 
+*/
