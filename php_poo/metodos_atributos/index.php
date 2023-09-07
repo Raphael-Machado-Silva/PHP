6b67d2e585e42d0b479531d6e80d52e343c24ad6
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-09-07 17:08:11 -0300

    Update index.php

--- php_poo/metodos_atributos/index.php
+++ php_poo/metodos_atributos/index.php
@@ -8,7 +8,12 @@ class Login{
     public static function verificaLogin(){ //criou método
             echo "O usuário ". self::$user." está logado!";
     }
+    public function sair(){
+        echo "<br>O usuário deslogou";
+    }
 }
 
 Login::$user = "admin";
 Login::verificaLogin(); // não é necessário instanciar a classe login -> $login = new Login();
+$Login02 = new Login();
+$Login02->sair();
