5ad38abb34f1e8401e641f39ada7ead4c52f2656
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-09-07 17:15:14 -0300

    Update index.php

--- php_poo/polimorfismo/index.php
+++ php_poo/polimorfismo/index.php
@@ -5,13 +5,21 @@ class Animal{
     public function Andar(){
         echo "O animal andou!";
     }
+
+    public function Correr(){
+        echo "<br>O animal correu!";
+    }
 }
 
 class Cavalo extends Animal{
     public function Andar(){ //ou seja o polimorfismo é rescrever um método já existente em uma classe parente
         echo "O cavalo andou!";
     }
+    public function correr02(){ //tmb é possível referenciar um método já existente dentro de outro
+        $this->Correr();
+    }
 }
 
 $animal = new Cavalo();
 $animal->Andar();
+$animal->correr02();
