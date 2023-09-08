6f5d1d0995a422a368dd1c0a030fad1d90ea476e
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-09-08 16:24:26 -0300

    Update index.php

--- php_poo/exceções/index.php
+++ php_poo/exceções/index.php
@@ -4,3 +4,27 @@
 //Ocorrência anormal que afeta o funcionamento da aplicação
 //Exception é a classe base para todas as Exceptions
 //message, code, file, line
+
+class Newsletter{
+    public function cadastrarEmail($email){
+
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            throw new Exception("Error", 1);
+        }
+            
+        else{
+            echo "Email cadastrado";
+        }
+    }
+}
+
+$newsletter = new Newsletter();
+
+try{ //tratando a exceção para não dar erro
+$newsletter->cadastrarEmail("contato@");
+} catch(Exception $e){
+    echo "Mensagem: " . $e->getMessage();
+    echo "<br>Código: " . $e->getCode();
+    echo "<br>Linha: " . $e->getLine();
+    echo "<br>Arquivo: " . $e->getFile();
+}
