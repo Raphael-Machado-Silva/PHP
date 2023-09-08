1c0bb220fb37d5f5f1c90de52ea41015a80290a2
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-09-08 16:32:16 -0300

    Update index.php

--- php_poo/associação/index.php
+++ php_poo/associação/index.php
@@ -1,3 +1,29 @@
 <?php
 //-------------ASSOCIAÇÃO/RELAÇÃO DE OBJETOS------------
 //Acontece quando um objeto "utiliza" outro, porém, sem que eles dependam um do outro
+
+class Pedido{
+    public $numero;
+    public $cliente;
+}
+
+class Cliente{
+    public $nome;
+    public $endereco;
+}
+
+$cliente_real = new Cliente();
+$cliente_real->nome = "Raphael";
+$cliente_real->endereco = "Gaivota";
+
+$pedido= new Pedido();
+$pedido->numero = "123";
+$pedido->cliente = $cliente_real; //a chave para entender o código está aqui
+
+    $dados = array(
+        "numero"=>$pedido->numero,
+        "nome_cliente"=>$pedido->cliente->nome,
+        "endereco_cliente"=>$pedido->cliente->endereco
+    );
+
+var_dump($dados);
