3676058d4e255e888b23e496604e0bbf6d4ae8bb
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-09-08 16:13:37 -0300

    Create index.php

---
+++ php_poo/exceções/index.php
@@ -0,0 +1,6 @@
+<?php
+//------------------------TRATAMENTO DE EXCEÇÕES--------------------------
+
+//Ocorrência anormal que afeta o funcionamento da aplicação
+//Exception é a classe base para todas as Exceptions
+//message, code, file, line
