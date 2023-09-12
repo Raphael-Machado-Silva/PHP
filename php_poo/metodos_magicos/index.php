2d0724c8a441a0762f04d15f8b7fbffef8375ccf
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-09-12 10:39:39 -0300

    Update index.php

--- php_poo/metodos_magicos/index.php
+++ php_poo/metodos_magicos/index.php
@@ -9,7 +9,18 @@
 
 class Pessoa{
     private $nome;
+
+    public function __set($nome, $valor){
+        $this->nome = $valor;
+    }
+
+    public function __get($nome){
+        return "O valor do atributo é: ".$this->$nome."<br>";
+    }
 }
 
 $pessoa = new Pessoa();
 $pessoa->nome = "Raphael";
+
+var_dump($pessoa);
+echo $pessoa->nome;
