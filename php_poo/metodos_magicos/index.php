8cb0bb8fcf22d677f24d067af7a6bad9c6fae947
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-09-12 10:44:16 -0300

    Update index.php

--- php_poo/metodos_magicos/index.php
+++ php_poo/metodos_magicos/index.php
@@ -24,3 +24,24 @@ $pessoa->nome = "Raphael";
 
 var_dump($pessoa);
 echo $pessoa->nome;
+
+
+// Sem saber aonde está o valor 
+
+class Dados{
+    private $dados = array();
+
+    public function __set($nome, $valor){
+        $this->dados[$nome] = $valor;
+    }
+
+    public function __get($nome){
+        return "O valor do atributo é: ".$this->dados[$nome]."<br>";
+    }
+}
+
+$pessoa02 = new Dados();
+$pessoa02->nome = "Helena";
+
+var_dump($pessoa02);
+echo $pessoa02->nome;
